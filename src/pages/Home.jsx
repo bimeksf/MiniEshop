3cc76a46cd3f9f7c6f72cf7706ba2f4f7eb6f0d8
bimeksf <d3disek@gmail.com>
2025-04-23 12:17:38 +0200

    feat: update Home and ProductList components with layout and content enhancements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return<div className="flex flex-col justify-center items-center">
+  
+  
+  <div className="flex flex-col m-auto">Home Page
+
+
+
+  <Link to={"/products"}>View All</Link>
+  </div>
+  </div>
+
 }
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -2,5 +2,49 @@
 import React from "react";
 
 export default function ProductList() {
-  return <div>Product List</div>;
+  return <div>
+    
+    
+    
+    <h1>
+      
+      Product List
+      
+      </h1>
+      
+      <div className="bg-slate-100 border-rose-50 border-2 flex flex-col justify-center items-center">
+        <img src="../assets/react.svg" alt="" />
+            <div>
+                <h2>Product</h2>
+                <p>Category</p>
+              </div>      
+              <div>
+                <img src="" alt="" />
+                  <span>2,256$</span>
+              </div>
+
+              <div className="flex flex-col">
+                  <h2>$1521</h2>
+                    <span>
+                    $1521
+                    </span>
+                    <span>
+                    -15%
+                    </span>
+
+              </div>
+
+      <button>Buy</button>
+      </div>
+      
+
+
+
+
+
+  </div>;
+
+
+
+
 }
