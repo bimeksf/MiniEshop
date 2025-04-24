39e8c4bed331469472ea141580784952dbfa53c3
bimeksf <d3disek@gmail.com>
2025-04-24 15:37:39 +0200

    Implement code changes to enhance functionality and improve performance

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -11,33 +11,40 @@ export default function ProductList() {
       Product List
       
       </h1>
-      
-      <div className="bg-slate-100 border-rose-50 border-2 flex flex-col justify-center items-center">
-        <img src="../assets/react.svg" alt="" />
-            <div>
-                <h2>Product</h2>
-                <p>Category</p>
-              </div>      
-              <div>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 ">
+
+
+
+      <div className="bg-slate-100 border-rose-50 border-2 text-left p-4">
                 <img src="" alt="" />
-                  <span>2,256$</span>
+            <div>
+                <h2 className="text-lg font-bold ">Product</h2>
+                <p className="text-sm text-gray-500">Category</p>
+              </div>      
+              <div className="flex gap-4">
+               <p>******</p>
+                  <span className="font-bold text-md">4.57</span>
               </div>
 
-              <div className="flex flex-col">
-                  <h2>$1521</h2>
-                    <span>
+              <div className="flex  flex-wrap  flex-col mb-3 ">
+                  <h2 className="text-red-500 font-bold text-lg">$1521</h2>
+                  <div>
+
+                    <span className="line-through mr-2 text-gray-500">
                     $1521
                     </span>
-                    <span>
+                    <span className="font-bold text-lg ">
                     -15%
                     </span>
+                  </div>
 
               </div>
 
-      <button>Buy</button>
+      <button className="p-4 bg-gray-400 rounded-lg">Buy</button>
       </div>
       
 
+      </div>
 
 
 
