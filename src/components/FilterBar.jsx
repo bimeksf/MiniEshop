e4a8b076cd4f23631275f34dc4ec3c21733fee98
bimeksf <d3disek@gmail.com>
2025-04-25 14:10:40 +0200

    feat: refactor FilterBar and ProductList components to enhance product filtering with dynamic data fetching, error handling, and improved UI

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,73 +1,59 @@
-import { useEffect,useState } from "react"
-import { fetchAllProducts } from "../utils/api"
+export default function FilterBar({
+  setSearchQuery,
+  searchQuery,
+  products,
+  selectedCategory,
+  setSelectedCategory,
+  setSortOrder,
+  sortOrder,
+}) {
+  const categories = [...new Set(products.map((item) => item.category))];
 
-export default function FilterBar(){
-  const [product, setProduct] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
+  return (
+    <div className="flex flex-col gap-4">
+      <form className="flex gap-4 items-center">
+        <select
+          onChange={(e) => setSortOrder(e.target.value)}
+          value={sortOrder}
+        >
+          <option value="all">default</option>
+          <option value="price-asc">Price (low to high)</option>
+          <option value="price-desc">Price (high to low)</option>
+          <option value="az">A-Z</option>
+        </select>
 
-  
+        <input
+          type="text"
+          placeholder="Search..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </form>
 
-useEffect(()=>{
-  setIsLoading(true);
+      <div>
+        <ul className="flex gap-4 flex-wrap mt-2">
+          <li
+            onClick={() => setSelectedCategory("all")}
+            className={`cursor-pointer ${
+              selectedCategory === "all" ? "font-bold underline" : ""
+            }`}
+          >
+            All items
+          </li>
 
-
-  async function getData() {
-    
-    try{
-      
-      const data= await fetchAllProducts()
-      setProduct(data);
-    } catch(error){
-      setError(error.message);
-    }
-    finally{
-      setIsLoading(false)
-    }
-    
-  }
-    getData()
-
-},[])
-
-
-const categories = [... new Set (product.map(item=> item.category))]
-
-    console.log(categories)
-
-
-
-    return (
-        <div>
-
-                <form action="">
-
-                        <select name="" id="">
-                                <option value="">low to high</option>
-                                <option value="">high to low</option>
-                        </select>
-
-                        <input type="text" />
-
-
-                </form>
-
-                        <ul>
-
-                    {categories.map(item=>{
-                        return<li>{item}</li>
-                        
-                    })}
-
-                    </ul>
-
-        </div>
-
-
-
-    )
-
-
-
-
+          {categories.map((cat) => {
+            return (
+              <li
+                key={cat}
+                onClick={() => setSelectedCategory(cat)}
+                className={cat === selectedCategory ? "font-bold" : ""}
+              >
+                {cat}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,33 +1,96 @@
-// src/pages/ProductList.jsx
 import React from "react";
-import ProductCard from "../components/ProductCard"
-
+import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
 import FilterBar from "../components/FilterBar";
+import { fetchAllProducts } from "../utils/api";
+
+import Loader from "../components/Loader";
+import Error from "../components/Error";
+
 export default function ProductList() {
-  return <div>
-    
-    
-    
-    <h1>
-      
-      Product List
-      
-      </h1>
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [sortOrder, setSortOrder] = useState("all");
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    async function getData() {
+      try {
+        const data = await fetchAllProducts();
+        setProducts(data);
+        setFilteredProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, []);
+
+  useEffect(() => {
+    let filtered = products;
+    {
+      /*  Sorting by price and az */
+    }
+    if (selectedCategory !== "all")
+      filtered = filtered.filter((p) => p.category === selectedCategory);
+
+    if (searchQuery) {
+      filtered = filtered.filter((p) =>
+        p.title.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+    }
+
+    {
+      /*  Sorting by price and az */
+    }
+    if (sortOrder === "price-asc") {
+      filtered = [...filtered].sort((a, b) => a.price - b.price);
+    } else if (sortOrder === "price-desc") {
+      filtered = [...filtered].sort((a, b) => b.price - a.price);
+    } else if (sortOrder === "az") {
+      filtered = [...filtered].sort((a, b) => a.title.localeCompare(b.title));
+    }
+
+    setFilteredProducts(filtered);
+  }, [selectedCategory, searchQuery, sortOrder, products]);
+
+  if (isLoading) return <Loader />;
+  if (error) return <Error />;
+  if (!isLoading && products.length === 0) return <div>No Products found</div>;
+
+  return (
+    <div>
+      <FilterBar
+        products={products}
+        selectedCategory={selectedCategory}
+        setSelectedCategory={setSelectedCategory}
+        setSortOrder={setSortOrder}
+        sortOrder={sortOrder}
+        setSearchQuery={setSearchQuery}
+        searchQuery={searchQuery}
+      />
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 ">
-
-        <FilterBar/>
-
-
-      
-
+        {filteredProducts.map((item) => {
+          return (
+            <ProductCard
+              key={item.id}
+              title={item.title}
+              price={item.price}
+              category={item.category}
+              image={item.image}
+              rating={item.rating}
+              id={item.id}
+            />
+          );
+        })}
       </div>
-
-
-
-
-  </div>;
-
-
-
-
+    </div>
+  );
 }
