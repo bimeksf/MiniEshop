4d2d306fda02aeb07660563de3f03fc959eefed6
bimeksf <d3disek@gmail.com>
2025-04-29 23:42:10 +0200

    feat: enhance Cart component with improved layout and styling; add quantity controls and icons for better user interaction

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,5 +1,8 @@
 import { useCartStore } from "../contex/cartStore";
 import { Link } from "react-router-dom";
+import { Plus,Minus, X } from 'lucide-react';
+
+
 export default function Cart() {
   const cartItems = useCartStore((state) => state.cartItems);
   const removeFromCart = useCartStore((state) => state.removeFromCart);
@@ -10,39 +13,59 @@ export default function Cart() {
   const totalPrice = cartItems.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
   return (
-    <div className="p-4">
+    <div className="p-4 h-screen ">
       <h1 className="text-2xl font-bold mb-4">Your Cart</h1>
 
       {cartItems.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <div className="space-y-4">
+        <div className="space-y-4 ">
           {cartItems.map((item) => (
-            <div key={item.id} className="flex justify-between items-center border-b pb-2">
-              <div>
-                <img className="w-20 h-20"  src={item.image} alt={item.title} />
-                <h2 className="text-lg font-semibold">{item.title}</h2>
-                <div className="flex justify-center items-center gap-2 border-2 border-black rounded-md">
-                <button className="p-2 hover:font-bold" onClick={()=>decreaseQuantity(item.id)}>-</button>
-                <span>{item.quantity}</span>
-                <button  className="p-2 hover:font-bold " onClick={()=>increaseQuantity(item.id)}>+</button>
-                </div>
+       <div key={item.id} className="flex flex-col border-b pb-2 gap-4 mb-4 bg-slate-50  rounded-xl p-4">
+       <div className="flex gap-4 w-full">
+         <img className="w-20 h-20 object-contain" src={item.image} alt={item.title} />
+           <h2 className="text-sm font-semibold ">{item.title}</h2>
+         <div className="flex flex-col flex-1">
+           <button className="self-end " onClick={() => removeFromCart(item.id)}>
+             <X />
+           </button>
+         </div>
+       </div>
 
-              </div>
+
+
+
+
+
+              {/*  bottom card */ }
+                  <div className="flex justify-between items-center">
+                  
+                <div className="flex justify-center items-center gap-2 border-2 border-black rounded-md">
+                <button className="p-1 hover:font-bold rounded-tl-md rounded-bl-md  bg-gray-200" onClick={()=>decreaseQuantity(item.id)}><Minus /></button>
+                <span>{item.quantity}</span>
+                <button  className="p-1 hover:font-bold bg-gray-200 rounded-tr-md rounded-br-md" onClick={()=>increaseQuantity(item.id)}><Plus /></button>
+                </div>
+                  
+
               <div className="text-right">
                 <p className="font-bold">${(item.price * item.quantity).toFixed(2)}</p>
               </div>
-              <button onClick={()=>removeFromCart(item.id)}>X</button>
+                   </div>
+
+
+
             </div>
             
           ))}
           <button onClick={clearCart} className="font-bold bg-slate-200 rounded-md p-4" >Clear</button>
           <div className="text-right mt-4">
+
+
             <h2 className="text-xl font-bold">Total: ${totalPrice.toFixed(2)}</h2>
           </div>
+          <Link to={"/checkout"} className="bg-slate-500 p-4 rounded-md">Proceed to Checkout</Link>
         </div>
       )}
-      <Link to={"/checkout"} className="bg-slate-500 p-4 rounded-md">Proceed to Checkout</Link>
     </div>
   );
 }
