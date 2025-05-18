f10f4d1507ebfd98149aa4f20b97fde198c08f44
bimeksf <d3disek@gmail.com>
2025-05-18 21:00:04 +0200

    refactor: replace BrowserRouter and Routes with createBrowserRouter and RouterProvider for improved routing structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,45 @@
-import './App.css'
+import './App.css';
 import React from "react";
-import { BrowserRouter, Route, Link ,Routes} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home from "./pages/Home"
-import ProductList from "./pages/ProductList"
-import ProductDetailPage from "./pages/ProductDetailPage"
-import LoginPage from "./pages/LoginPage"
-import Cart from "./pages/Cart"
-import Checkout from "./pages/CheckoutPage"
-import CheckoutPage from "./pages/CheckoutPage"
-import NotFoundPage from "./pages/NotFoundPage"
-import WhishList from "./pages/WhishListPage"
-import Layout from "./components/Layout"
-import Auth from "./components/Auth"
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import ProductDetailPage from "./pages/ProductDetailPage";
+import LoginPage from "./pages/LoginPage";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/CheckoutPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import WhishList from "./pages/WhishListPage";
+import Layout from "./components/Layout";
+import Auth from "./components/Auth";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "products", element: <ProductList /> },
+      { path: "products/:id", element: <ProductDetailPage /> },
+      { path: "cart", element: <Cart /> },
+      { path: "checkout", element: <Checkout /> },
+      { path: "login", element: <LoginPage /> },
+
+
+      {
+        element: <Auth />,
+        children: [
+          { path: "payment", element: <Checkout /> }, 
+          { path: "wishlist", element: <WhishList /> },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-
-  return (
-<BrowserRouter>
-  <Routes>
-    <Route path="/" element={<Layout />} >
-      <Route index  element={<Home />} />
-      <Route path="products" element={<ProductList />} />
-      <Route path="products/:id" element={<ProductDetailPage />} />
-      <Route path="cart" element={<Cart />} />
-      <Route path="checkout" element={<Checkout />} />
-      <Route path="login" element={<LoginPage />} />
-
-
-
-    <Route element={<Auth/>}>
-        <Route path="payment" element={<CheckoutPage />} />
-        <Route path="wishlist" element={<WhishList />} />
-        
-
-    </Route>
-    <Route path="*" element={<NotFoundPage/>} />
-    </Route>
-
-  </Routes>
-</BrowserRouter>
-
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
